8df683efe617b465a135fdfdc43ffdee8880affa
taiki naya <taiki780528@gmail.com>
2021-09-07 11:42:45 +0000

    agenda_mailメソッドを定義

--- app/mailers/agenda_mailer.rb
+++ app/mailers/agenda_mailer.rb
@@ -1,2 +1,9 @@
 class AgendaMailer < ApplicationMailer
+  default from: 'from@example.com'
+
+  def agenda_mail(email, agenda)
+    @email = email
+    @agenda = agenda
+    mail to: @email, subject: "Agenda deleted"
+  end
 end
